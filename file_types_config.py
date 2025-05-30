838862a7a1d48be0628b58ef18312f9c69e06b50
fatih <rinqtf@gmail.com>
2025-05-31 01:31:21 +0300

    add file types

---
+++ file_types_config.py
@@ -0,0 +1,8 @@
+FILE_TYPES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp"],
+    "Documents": [".pdf", ".docx", ".txt", ".md"],
+    "Spreadsheets": [".xls", ".xlsx", ".csv"],
+    "Archives": [".zip", ".rar", ".tar", ".gz"],
+    "Executables": [".exe", ".sh", ".bat"],
+    "Others": [],
+}
