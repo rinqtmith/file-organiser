a94a84fc19feac2d1645ea614508fba988905410
Fatih Avcı <rinqtf@gmail.com>
2025-06-06 02:20:59 +0300

    Feat cli support (#12)
    * add argparse to get path
    
    * add conditions to path
    
    * use Path from pathlib

--- file_organiser.py
+++ file_organiser.py
@@ -1,33 +1,61 @@
+import argparse
 import os
 import shutil
 
-from file_types_config import FILE_TYPES
+from pathlib import Path
 
-# use hard coded path for testing purposes
-PATH = "../example_folder"
+from file_types_config import FILE_TYPES
 
 
 def main():
-    for filename in os.listdir(PATH):
-        filename_path = os.path.join(PATH, filename)
-        if os.path.isfile(filename_path):
-            _, ext = os.path.splitext(filename)
+    parser = argparse.ArgumentParser(description="Organise files in a directory")
+    parser.add_argument(
+        "path", help="Path to the directory to organise files in", type=str
+    )
+    args = parser.parse_args()
+    PATH = Path(args.path).resolve()
+    cwd = Path.cwd()
+
+    if not PATH.exists():
+        print(f"Path '{PATH}' does not exist.")
+        return
+
+    if not PATH.is_dir():
+        print(f"Path '{PATH}' is not a directory.")
+        return
+
+    if PATH.is_relative_to(cwd):
+        print(
+            "Warning: The provided path is relative to the current working directory."
+        )
+        return
+
+    if cwd.is_relative_to(PATH):
+        print(
+            "Warning: The current working directory is a subdirectory of the provided path."
+        )
+        return
+
+    file_list = list(PATH.iterdir())
+
+    for filename in file_list:
+        if filename.is_file():
             for file_type in FILE_TYPES:
-                if ext.lower() in FILE_TYPES[file_type]:
-                    folder_path = os.path.join(PATH, file_type)
+                if filename.suffix.lower() in FILE_TYPES[file_type]:
+                    folder_path = PATH.joinpath(file_type)
                     try:
                         os.mkdir(folder_path)
                     except FileExistsError:
                         pass
-                    shutil.move(filename_path, folder_path)
+                    shutil.move(filename, folder_path.joinpath(filename.name))
                     break
             else:
-                other_path = os.path.join(PATH, "Other")
+                other_path = PATH.joinpath("Other")
                 try:
                     os.mkdir(other_path)
                 except FileExistsError:
                     pass
-                shutil.move(filename_path, other_path)
+                shutil.move(filename, other_path.joinpath(filename.name))
 
 
 if __name__ == "__main__":
